1448ed4c4be9ec37877b645f5e7592cf72764022
Plozada <palozadag@gmail.com>
2023-05-28 15:09:35 -0500

    Probando app a Katronix

--- uno.py
+++ uno.py
@@ -32,74 +32,26 @@ def getBotSearchOffer(driver: webdriver.Chrome) -> str:
     driver.set_window_size(1280, 1024)
     print('ingresaaa')
     acciones = ActionChains(driver)
-    driver.get("https://www.toolsqa.com/")
+    driver.get("https://www.ktronix.com/")
     das = driver.current_url
     print(das)
-    # Realizar acciones en la página
-   
-    menu = driver.find_element(By.XPATH,"(//span[@class='bar-long'])[1]")
-    menu.click()
-    qa = driver.find_element(By.XPATH,"(//div[contains(.,'QA Practices')])[5]")
-    acciones.move_to_element(qa).perform()
-    testing = driver.find_element(By.XPATH,"(//a[@href='/software-testing/software-testing-tutorial/'])[3]")
-    testing.click()
-
-    caption = driver.find_element(By.XPATH,"(//h1[contains(@class,'title')])[1]")
-    time.sleep(1)
-    print(caption.text + 'holaaaaaaaaaaaa')
-    mivariable=caption.text
-    print('Exitoso')
+    cel = driver.find_element(By.XPATH,"(//a[@title='Celulares'])[1]")
+    acciones.move_to_element(cel).perform()
+    celu = driver.find_element(By.XPATH,"//a[@data-subcategory='Celulares']")
+    celu.click()
+    #order = driver.find_element(By.XPATH,"//div[contains(@class,'float-select js-float-select js-float-group full-width active-click')]")
+    #order.click()
+    #driver.execute_script("window.scrollTo(0, 200")
+    #menor = driver.find_element(By.XPATH,"//li[contains(.,'Precio: menor a mayor')]")
+    #menor.click()
+    time.sleep(5)
+    caption = driver.find_element(By.XPATH,"//div[@class='algolia-stats'][contains(.,'Mostrando 25 de 335 resultados')]")
+    print(caption.text)
+    mivariable = caption.text
     return(mivariable)
 
 
-'''def getBotSearchOffer(driver: webdriver.Chrome)-> dict:
-    driver.set_window_position(0, 0)
-    driver.set_window_size(1280, 1024)
-    data ={}
-    acciones = ActionChains(driver)
-    driver.get("https://www.exito.com/")
-    time.sleep(5)
-    menu = driver.find_element(By.ID,"category-menu")
-    menu.click()
-    time.sleep(8)
-    celulares = driver.find_element(By.ID,"undefined-nivel2-Celulares y accesorios")
-    acciones.move_to_element(celulares).perform()
-    smarp = driver.find_element(By.ID,'Categorías-nivel2-Smartphones')
-    smarp.click()
-    time.sleep(8)
-    order = driver.find_element(By.XPATH,'//span[@class="vtex-search-result-3-x-orderByText c-muted-2"]')   
-    order.click()
-    time.sleep(8)
-    moreH = driver.find_element(By.XPATH,'//button[contains(.,"Descuento")]')
-    moreH.click()
-    time.sleep(10)
-    referencia = []
-    valor =[]
 
-    for i in range(3) :
-        valRef=str(i+1)
-        valVal=str((i+1)*2)
-        ref = driver.find_element(By.XPATH,"(//span[@class='vtex-product-summary-2-x-productBrandName'])["+valRef+"]")
-        val = driver.find_element(By.XPATH,"(//span[contains(@class,'exito-vtex-components-4-x-currencyContainer')])["+valVal+"]")
-        referencia.append(ref.text)
-        valor.append(val.text)        
-    data['Exito']=[referencia, valor]
-    print(data)
-    return(data)'''
-
-'''def getBotSearchOffer(driver: webdriver.Chrome)-> str:
-    driver.set_window_position(0, 0)
-    driver.set_window_size(1280, 1024)
-  
-   
-    driver.get("https://www.exito.com/")
-    time.sleep(5)
-    driver.maximize_window()
-    time.sleep(5)
-    menu = driver.find_element(By.XPATH,"//span[contains(.,'Super ofertas')]")
-    print(menu.text)
-
-    return(menu.tex)'''
 
 def doBackgroundTask(inp):
     print("Doing background task")
