e335c58055bc8f7a4ad671282fa1d4b9a6df9e92
Plozada <palozadag@gmail.com>
2023-06-30 18:29:47 -0500

    Update Bot_New_Products

--- page_totto.py
+++ page_totto.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.by import By
 
 class TottoHome ():
     menu_on = (By.ID, 'menu_on')
-    new_collection = (By.XPATH, "//a[contains(.,'Nueva colección')]")
+    close_adv = (By.XPATH, "//div[@class='bton-cerrar']/span")
     product_info = (By.XPATH, "//div[@class='_product-info']/h3/span[1]")
 
     @staticmethod
     def item_select(item):
-        return (By.XPATH, "(//img[@class=' b-error'])["+item+"]")
+        return (By.XPATH, "//li[@class='level2']["+item+"]")
     
     @staticmethod
     def filter_category(category):
