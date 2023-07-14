ef0593ce827f44e71efdebf8ed060e04cc0d0c13
Plozada <palozadag@gmail.com>
2023-07-13 23:14:57 -0500

    Update appExecution.py

--- appExecution.py
+++ appExecution.py
@@ -7,6 +7,7 @@ from page_totto import *
 from page_comput import *
 from actions import *
 
+mivariable =''
 
 def createDriver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
