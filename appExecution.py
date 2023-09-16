168f73832b6287d93e8564e2044ff53810ca2a36
Plozada <palozadag@gmail.com>
2023-09-16 02:57:11 -0500

    Update headers_´B

--- appExecution.py
+++ appExecution.py
@@ -14,7 +14,7 @@ def createDriver() -> webdriver.Chrome:
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")    
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.1000.0 Safari/537.36")
+    chrome_options.add_argument("user-agent=Chrome/100.0.1000.0")
     prefs = {"profile.managed_default_content_settings.images":2}
     chrome_options.headless = True
     chrome_options.add_experimental_option("prefs", prefs)
