098e0dacbef7ba128f549e689b43f33640a8b4a9
Plozada <palozadag@gmail.com>
2023-11-09 23:47:48 -0500

    Update Selectors page_comput

--- page_comput.py
+++ page_comput.py
@@ -12,8 +12,8 @@ class CompuVacancies():
     alert_button = (By.XPATH, "//button[contains(.,'Ahora no')]")
     option_date = (By.XPATH, "//p[contains(.,'Fecha')]")
     jobs = (By.XPATH, "(//a[@class='js-o-link fc_base'])")
-    places = (By.XPATH, "(//p[@class='fs16 fc_base mt5 mb5'])")
-    how_Long= (By.XPATH,"(//p[@class='fs13 fc_aux'])")
+    places = (By.XPATH, "(//p[@class='fs16 fc_base mt5'])")
+    how_Long= (By.XPATH,"(//p[@class='fs13 fc_aux mt15'])")
 
     @staticmethod
     def filt_date(filter_date):
